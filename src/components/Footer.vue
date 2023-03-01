7c5877b123412f0e59fe3676d13e4a4bd40e5993
Sakhe12 <sakhemzolisa@gmail.com>
2023-03-01 16:32:10 +0200

    update

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -6,8 +6,7 @@
                 <p>Ph: 063 681 3151</p>
                 <p>Email: johnnoad04@gmail.com</p>
               </div>
-            <p class="text-muted"><i class="bi bi-geo-alt-fill"></i>24 New Rest, Gugulethu,</p>
-    
+            <p class="text-muted"><i class="bi bi-geo-alt-fill"></i>24 New Rest, Gugulethu,</p> 
             <p class="text-dark text-success">Copyright &copy; 2023 by Sakhe Mzolisa & John-Bruce Node</p>
             </footer>/*
     </div>
--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,19 +1,14 @@
 <template>
     <div>
-        <table class="table table-hover table-striped">
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Price</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="product in products" :key="product"> 
-                    <td>{{product.productName}}</td>
-                    <td>R{{ product.productPrice}}</td>
-                </tr>
-            </tbody>
-        </table>
+        <div class="card" style="width: 18rem;" v-for="product in products" :key="product">
+            <img src="" class="card-img-top" alt="">
+            <div class="card-body">
+              <h5 class="card-title">{{product.productName}}</h5>
+              <p class="card-text">{{ product.productDescription}}</p>
+              <p class="card-text">{{ product.productPrice}}</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
     </div>
 </template>
 <script>
